666a839c351a5053db7ce964ac3b1aeb65942ed2
Graham Neubig <neubig@gmail.com>
2014-01-30 12:45:41 +0900

    Updated visualization script

--- script/visualize.pl
+++ script/visualize.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+use utf8;
 use List::Util qw( max min sum );
 use strict;
 binmode STDOUT, ":utf8";
@@ -12,8 +13,19 @@ if(@ARGV < 3 or @ARGV > 5) {
 open EFILE, "<:utf8", $ARGV[0] or die "$ARGV[0]: $!";
 open FFILE, "<:utf8", $ARGV[1] or die "$ARGV[1]: $!";
 open AFILE, "<:utf8", $ARGV[2] or die "$ARGV[2]: $!";
-my $EWIDTH = $ARGV[3] ? $ARGV[3] : 1;
-my $FWIDTH = $ARGV[4] ? $ARGV[4] : 1;
+
+sub asciilength {
+    my $str = shift;
+    my $ret = 0;
+    for(split(//, $str)) {
+        if(/(\p{InHiragana}|\p{InKatakana}|\p{InCJKUnifiedIdeographs}|[ａ-ｚＡ-Ｚ０-９！”＃＄％＆’（）「」『』、。々])/) {
+            $ret += 2;
+        } else {
+            $ret += 1;
+        }
+    }
+    return $ret;
+}
 
 my ($i, $j);
 my (@actives, $actmax, $estr, $fstr, $astr);
@@ -22,28 +34,29 @@ while($estr = <EFILE> and $fstr = <FFILE> and $astr = <AFILE>) {
     # if(not $astr) { next; print "\n"; }
     my %active = map { my ($e,$f) = split(/-/); my $id = "".$e."-".$f; $id => 1 } split(/ /,$astr);
     my @e = split(/ /,$estr);
-    my $elen = max( map { length } @e );
+    my $elen = max( map { asciilength($_) } @e );
     my @f = split(/ /,$fstr);
-    my $flen = max( map { length } @f );
+    my $flen = max( map { length($_) } @f );
     for(0 .. $flen) {
-        print " " for(0 .. $elen*$EWIDTH);
+        print " " for(0 .. $elen);
         my $pos = $flen-$_;
         for(@f) {
             if($pos<length($_)) {
-                print substr($_,length($_)-$pos-1,1);
+                my $str = substr($_,length($_)-$pos-1,1);
+                if(asciilength($str) == 1) { print " "; }
+                print $str;
             } else {
-				print " " for( 1 .. $FWIDTH);
+				print "  ";
             }
         }
         print "\n";
     }
     foreach my $i (0 .. $#e) {
-        print " " for(1 .. ($elen-length($e[$i]))*$EWIDTH);
+        print " " for(1 .. ($elen-asciilength($e[$i])));
         print "$e[$i] ";
         foreach my $j (0 .. $#f) {
             my $id = "$i-$j";
-            print ($active{$id}?"X":".");
-			print " " for(2 .. $FWIDTH);
+            print ($active{$id}?" X":" .");
         }
         print "\n";
     }
